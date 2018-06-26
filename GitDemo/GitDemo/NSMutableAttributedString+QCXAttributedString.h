c440dd5c23bce179fe67aa8c5fbb00446ccebbe4
乔春晓 <qiaochunxiao@qiaochunxiaodeMacBook-Pro-2.local>
2018-06-26 18:36:32 +0800

    自己写了一个改变字体的类

---
+++ GitDemo/GitDemo/NSMutableAttributedString+QCXAttributedString.h
@@ -0,0 +1,13 @@
+//
+//  NSMutableAttributedString+QCXAttributedString.h
+//  GitDemo
+//
+//  Created by 乔春晓 on 2018/6/26.
+//  Copyright © 2018年 乔春晓. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSMutableAttributedString (QCXAttributedString)
+- (NSMutableAttributedString * (^)(NSString *,NSDictionary<NSString *,id> *))add;
+@end
--- GitDemo/GitDemo/ViewController.m
+++ GitDemo/GitDemo/ViewController.m
@@ -7,18 +7,39 @@
 //
 
 #import "ViewController.h"
+#import <Masonry.h>
+#import "NSMutableAttributedString+QCXAttributedString.h"
 
 @interface ViewController ()
-
+@property (nonatomic, strong) UILabel *label;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    _label = [[UILabel alloc] init];
+    [self.view addSubview:_label];
+//    _label.text = @"哈哈哈";
+//    _label.backgroundColor = [UIColor redColor];
+    
+    [_label mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.center.mas_equalTo(self.view);
+        make.width.mas_equalTo(200);
+        make.height.mas_equalTo(60);
+    }];
+    
+    NSMutableAttributedString *str = [[NSMutableAttributedString alloc] init];
+    str.add(@"小样",@{
+                    NSForegroundColorAttributeName:[UIColor redColor],
+                    NSFontAttributeName :[UIFont systemFontOfSize:14],
+                    });
+    str.add(@"hhhh样",@{
+                    NSForegroundColorAttributeName:[UIColor purpleColor],
+                    NSFontAttributeName :[UIFont systemFontOfSize:30],
+                    });
+    _label.attributedText = str;
 }
-
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
